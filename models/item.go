f5c9583387348645b220159c3116dcbb339ae192
Lucas Floriani <lucas.floriane@gmail.com>
2018-04-07 22:17:59 -0300

    Add field in model item to verify if the item can sell in npcs

--- models/item.go
+++ models/item.go
@@ -4,10 +4,11 @@ import "github.com/jinzhu/gorm"
 
 type Item struct {
 	gorm.Model
-	Name      string `gorm:"not null" json:"item-name"`
-	Type      ItemType
-	SubType   []ItemSubtype
-	PriceBuy  float64 `gorm:"not null" json:"item-pricebuy"`
-	PriceSell float64 `gorm:"not null" json:"item-pricesell"`
-	Weight    float64 `gorm:"not null" json:"item-weight"`
+	Name       string `gorm:"not null" json:"item-name"`
+	Type       ItemType
+	SubType    []ItemSubtype
+	PriceBuy   float64 `gorm:"not null" json:"item-pricebuy"`
+	PriceSell  float64 `gorm:"not null" json:"item-pricesell"`
+	NpcCanSell bool    `gorm:"not null" json:"item-npccansell"`
+	Weight     float64 `gorm:"not null" json:"item-weight"`
 }
