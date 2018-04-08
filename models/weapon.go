c18e875c840cbcbd4bb08ead4c51b7f61f052b7e
Lucas Floriani <lucas.floriane@gmail.com>
2018-04-07 23:16:31 -0300

    Add model weapon fields

--- models/weapon.go
+++ models/weapon.go
@@ -1,1 +1,16 @@
 package models
+
+import "github.com/jinzhu/gorm"
+
+type Weapon struct {
+	gorm.Model
+	WeaponLevel       uint `gorm:"not null" json:"weapon-weapon-level"`
+	LevelRequired     uint `gorm:"not null" json:"weapon-level-required"`
+	AttributeRequired []Attribute
+	CanUse            []Class // What class can use
+	Range             uint    `gorm:"not null" json:"weapon-range"`
+	PhysicalDamage    Dice
+	MagicDamage       Dice
+	ExtraAttribute    []Attribute
+	Slot              uint `gorm:"not null" json:"weapon-slot"`
+}
